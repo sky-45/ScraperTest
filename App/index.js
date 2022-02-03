1bd91661e7465a3134f96fd59ba86bf83ab32437
Johan Agustin Barrera Alderete <ja_johan@hotmail.com>
2022-02-03 17:35:43 -0500

    implementing all of profile related functionsinto the class

--- App/index.js
+++ App/index.js
@@ -23,7 +23,7 @@ async function scrapProfileLinkedin() {
   
         }
       }
-    async #wait(seconds){
+    static async wait(seconds){
       return new Promise(function(resolve,reject){
         setTimeout(function(){
           resolve()
@@ -34,80 +34,45 @@ async function scrapProfileLinkedin() {
     async getContactCard(){
         const {profileUrl,email} = this.cssselectors.contactCard;
         document.getElementById("top-card-text-details-contact-info").click();
-        await this.#wait(0.5);
+        await Profile.wait(0.5);
         const profileUrlElement = await document.querySelector(String(profileUrl))?.innerText;
         const emailLinkElement = await document.querySelector(String(email))?.innerText;
         const [closeButton] = document.getElementsByClassName('artdeco-button');
         await closeButton.click();
-        await this.#wait(1);
+        await Profile.wait(0.5);
   
         return  {
                   profileUrl: profileUrlElement,
                   email: emailLinkElement
                 }
       }
-  }
+    
+    static async autoscroll(cssselectors){
+        const element = document.querySelector(cssselectors);
+        while(element){
+          let maxScrollTop = document.body.clientHeight - window.innerHeight;
+          let elementScrollTop = document.querySelector(cssselectors).offsetHeight;
+          let currentScrollTop = window.scrollY;
   
-  // getting data
-    const cssselectors = {
-      name:'h1',
-      contactCard:{
-        profileUrl:'.ci-vanity-url > .pv-contact-info__ci-container > .pv-contact-info__contact-link',
-        email:'.ci-email > .pv-contact-info__ci-container > .pv-contact-info__contact-link'
-
+          if(maxScrollTop <= currentScrollTop+20 || elementScrollTop <= currentScrollTop) break;
+          await Profile.wait(0.02);
+  
+          let newScrollTop = Math.min(currentScrollTop + 20,maxScrollTop);
+  
+          window.scrollTo(0,newScrollTop);
+  
+        }
       }
-    }
-
-    async function wait(seconds){
-      return new Promise(function(resolve,reject){
-        setTimeout(function(){
-          resolve()
-        },seconds*1000)
-      })
-    }
-
-    async function autoscroll(cssselectors){
-      const element = document.querySelector(cssselectors);
-      while(element){
-        let maxScrollTop = document.body.clientHeight - window.innerHeight;
-        let elementScrollTop = document.querySelector(cssselectors).offsetHeight;
-        let currentScrollTop = window.scrollY;
-
-        if(maxScrollTop <= currentScrollTop+20 || elementScrollTop <= currentScrollTop) break;
-        await wait(0.05);
-
-        let newScrollTop = Math.min(currentScrollTop + 20,maxScrollTop);
-
-        window.scrollTo(0,newScrollTop);
-
-      }
-    }
-
-    function getContactInfo(){
-      const {name} = cssselectors;
-      const nameElement = document.querySelector(name);
       
-      const urlProfile = window.location.href;
-      return  {
-                name: nameElement?.textContent,
-                url: urlProfile
-              }
-    }
 
-    //const info = getContactInfo();
-    //await autoscroll('body');
-    //await wait(0.5);
-    //const contact_Card = await getContactCard(); 
-    //await wait(0.5);
-    //console.log(contact_Card);
-
-
+  }
     
     const profile = new Profile();
     const contact_Card = await profile.getContactCard();
+    await Profile.autoscroll('body');
     console.log(contact_Card);
     
-    //TODO: compelte rest of data parameters
+    //TODO: complete rest of data parameters
 
     //formating data
     //  we have to format the data so that it's ready to push to the database
